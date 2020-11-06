9c97a75c77833d3e0e23e87d13f07c954ffee2cd
mityadv <mityadv@gmail.com>
2020-11-06 23:43:01 +0200

    добавляет task pug

---
+++ gulp/tasks/pugToHtml.js
@@ -0,0 +1,18 @@
+const config = require(`../config`);
+const gulp = require(`gulp`);
+const pug = require(`gulp-pug`);
+const plumber = require(`gulp-plumber`);
+const pugLinter = require(`gulp-pug-linter`);
+const htmlValidator = require('gulp-w3c-html-validator')
+const bemValidator = require('gulp-html-bem-validator')
+
+module.exports = function pugToHtml() {
+  return gulp
+    .src(config.pug.pages)
+    .pipe(plumber())
+    .pipe(pugLinter({reporter: `default`}))
+    .pipe(pug({ pretty: true }))
+    .pipe(htmlValidator())
+    .pipe(bemValidator())
+    .pipe(gulp.dest(config.dist));
+}

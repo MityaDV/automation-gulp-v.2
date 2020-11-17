1f1ac0d1be6fe12f66e46c21cc727ee10e2f4f4d
mityadv <mityadv@gmail.com>
2020-11-17 21:50:24 +0200

    create task styleDev

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require(`gulp`);
 const pugToHtml = require(`./gulp/tasks/pugToHtml`);
 const styles = require(`./gulp/tasks/styles`);
+const stylesDev = require(`./gulp/tasks/stylesDev`); // добавить вызов этого таска в series
 
 module.exports.start = gulp.series(pugToHtml, styles)
